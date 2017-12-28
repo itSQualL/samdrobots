9fe9d9ab0ce6caa49631f3545874eb1d4a5b9eed
itsquall <sergioacaraballo@gmail.com>
2017-12-28 17:36:47 +0100

    Add container folder
    Container folder is used to manage the distributed part that allows
    to create a dict and saves all necessary proxies

---
+++ container/container.py
@@ -0,0 +1,38 @@
+import Ice
+Ice.loadSlice('services.ice')
+
+import services
+
+class ContainerI(services.Container):
+    """
+    Container interface implementation
+    """
+
+    def __init__(self):
+        self.game_prxs = dict()
+
+    def link(self, key, proxy, current=None):
+        """
+        link adds a new entry to the game_prxs dict.
+        If it already exists, it will throw an AlreadyExists exception
+        """
+        if key in self.game_prxs: raise services.AlreadyExists(key)
+
+        self.game_prxs[key] = proxy
+        print("link: {0} -> {1}".format(key, proxy))
+
+    def unlink(self, key, current=None):
+        """
+        unlink removes from game_prxs dict an existing key.
+        If it doesn't have the passed key, it will throw an NoSuchKey exception
+        """
+        if not key in self.game_prxs: raise services.NoSuchKey(key)
+
+        del(self.game_prxs[key])
+        print("unlink: {0}".format(key))
+
+    def list(self, current=None):
+        """
+        list all the keys for game_prxs
+        """
+        return self.game_prxs
